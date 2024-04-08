a144e591c42f90ca120e87b1f0c41adfb8c66c4c
zbb <1395691724@qq.com>
2024-04-08 06:27:06 +0000

    sumbit

--- ts_benchmark/baselines/self_implementation/__init__.py
+++ ts_benchmark/baselines/self_implementation/__init__.py
@@ -7,7 +7,6 @@ __all__ = [
 
 
 from ts_benchmark.baselines.self_implementation.LOF.lof import LOF
-from ts_benchmark.baselines.self_implementation.VAR.VAR import VAR_model
 from ts_benchmark.baselines.self_implementation.DCdetector.DCdetector import DCdetector
 from ts_benchmark.baselines.self_implementation.Anomaly_trans.AnomalyTransformer import AnomalyTransformer
 
